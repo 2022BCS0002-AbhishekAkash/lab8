428b58363dfc8f3129beb7655cb2f75dca872aaf
2022BCS0002-AbhishekAkash <abhishek22bcs2@iiitkottayam.ac.in>
2026-03-16 23:22:22 +0530

    version 1 pipeline

--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ import json
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import root_mean_squared_error, r2_score
 
 # Load dataset
 df = pd.read_csv("data/housing.csv")
@@ -32,7 +32,7 @@ model.fit(X_train, y_train)
 preds = model.predict(X_test)
 
 # Metrics
-rmse = mean_squared_error(y_test, preds, squared=False)
+rmse = root_mean_squared_error(y_test, preds)
 r2 = r2_score(y_test, preds)
 
 metrics = {
